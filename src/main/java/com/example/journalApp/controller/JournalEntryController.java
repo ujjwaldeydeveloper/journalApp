afe856ab6ce99d7f80ca79d76c99c718cb65cbeb
Ujjwal Dey <ujjwaldey55.ud@gmail.com>
2025-09-02 15:58:51 +0530

    Creating TEST Api

---
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,46 @@
+package com.example.journalApp.controller;
+
+import com.example.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    public Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry) {
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId) {
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId) {
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJounalById(@PathVariable Long myId, @RequestBody JournalEntry myEntry) {
+        return journalEntries.put(myId, myEntry);
+    }
+
+
+
+
+}
