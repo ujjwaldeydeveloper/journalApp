2de256eb92cb39938338e6893db7cf1e7b09b4e0
Ujjwal Dey <ujjwaldey55.ud@gmail.com>
2025-09-08 09:36:52 +0530

    missed pojo class updated

--- src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -1,9 +1,26 @@
 package com.example.journalApp.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+
+@Document(collation = "journal_entries")
 public class JournalEntry {
-    private long id;
+    @Id
+    private String id;
     private String title;
     private String content;
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     public String getTitle() {
         return title;
@@ -21,11 +38,11 @@ public class JournalEntry {
         this.content = content;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
