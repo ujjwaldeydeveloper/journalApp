a7f59b293e4aade9dccbaf7a97675fbfd1dfbe5e
Ujjwal Dey <ujjwaldey55.ud@gmail.com>
2025-09-10 09:34:05 +0530

    lombok dependency added to remove the boilareplate pojo class code

--- src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.example.journalApp.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,43 +9,12 @@ import java.util.Date;
 
 
 @Document(collection ="journalEntry")
+@Data
 public class JournalEntry {
     @Id
     private Object id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Object getId() {
-        return id;
-    }
-
-    public void setId(Object id) {
-        this.id = id;
-    }
 
 }
