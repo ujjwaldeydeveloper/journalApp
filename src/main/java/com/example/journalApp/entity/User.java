e42dcc8aa53f7d88dd9f112c2fae28778bf61755
Ujjwal Dey <ujjwaldey55.ud@gmail.com>
2025-12-09 10:33:33 +0530

    Null pointer exception for role resolved and security check by username properly. api call als whould be with proper object body

--- src/main/java/com/example/journalApp/entity/User.java
+++ src/main/java/com/example/journalApp/entity/User.java
@@ -26,5 +26,5 @@ public class User {
     private String password;
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
-    private List<String> roles;
+    private List<String> roles = new ArrayList<>();
 }
--- src/main/java/com/example/journalApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/journalApp/service/UserDetailsServiceImpl.java
@@ -18,12 +18,31 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    /*
+    Security Filter Intercepts: Before the request reaches your controller
+    (UserController, PublicController, etc.), it's intercepted by Spring
+    Security's filter chain.
 
+    Authentication Attempt: The filter chain's primary job is to establish
+    the identity of the caller. It looks for authentication details,
+    typically a JWT in an Authorization header or a session cookie.
+
+    Username Extraction: If a token is found, Spring Security
+    validates it and extracts the username.
+
+    User Details Loading: It then calls the loadUserByUsername method of your
+    UserDetailsServiceImpl. This is the crucial step. Spring Security needs
+    to get the user's latest details (especially roles and password) from the
+    database to build a complete Authentication object. This object represents
+    the currently logged-in user for this specific request.
+
+    Security Context: This Authentication object is placed in the SecurityContextHolder,
+    making it available throughout your application (which is why SecurityContextHolder.
+    getContext().getAuthentication() works in your controllers).
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String use = authentication.getName();
-        Optional<User> user = userRepository.findByName(use);
+        Optional<User> user = userRepository.findByName(username);
         if(user.isPresent()) {
             UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                     .username(user.get().getName())
