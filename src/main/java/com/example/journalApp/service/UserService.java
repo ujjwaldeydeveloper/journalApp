7e9cc7222eee2087c45be8929e0983aa07634934
Ujjwal Dey <ujjwaldey55.ud@gmail.com>
2026-01-13 10:44:37 +0530

    using @Slf4j annotation for logging

--- src/main/java/com/example/journalApp/service/UserService.java
+++ src/main/java/com/example/journalApp/service/UserService.java
@@ -2,9 +2,8 @@ package com.example.journalApp.service;
 
 import com.example.journalApp.entity.User;
 import com.example.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -14,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -21,9 +21,6 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-
 
     public void saveEntry(User user) {
         userRepository.save(user);
@@ -37,7 +34,7 @@ public class UserService {
             userRepository.save(user);
             return true;
         } catch (Exception e) {
-            logger.info("Invaild User or Duplicate user detected while saving for {} :", user.getName(), e);
+            log.info("Invaild User or Duplicate user detected while saving for {} :", user.getName(), e);
             return false;
         }
     }
